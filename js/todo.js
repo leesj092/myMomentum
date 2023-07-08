0d15ee6d4750246a760892eb31e94ad7a795c530
leesj092 <slee887@wisc.edu>
2023-07-09 00:43:46 +0900

    finished pre-polished basic functionality

--- js/todo.js
+++ js/todo.js
@@ -1,2 +1,54 @@
 const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    const newTodoObj = {
+        text: newTodo, 
+        id: Date.now(),
+    };
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj);
+    saveTodos();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    span.innerText = newTodo.text ;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    li.remove();
+    saveTodos();
+}
+
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY)
+
+if (savedTodos) {
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
