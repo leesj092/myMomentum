d521c8984633eceeb7b969dc0e650f536592375a
leesj092 <slee887@wisc.edu>
2023-07-16 20:46:31 +0900

    added weather functionality

--- js/formCss.js
+++ js/formCss.js
@@ -2,29 +2,30 @@ const forms = document.getElementsByClassName("form");
 
 Array.from(forms).forEach((form) => {
   const input = form.querySelector('input[type="text"]');
-  input.addEventListener("input, submit", () => {
-    const font = window.getComputedStyle(input).font;
-    const enteredText = input.value;
-    const textWidth = getTextWidth(enteredText, font) || getTextWidth(input.placeholder, font);
-    console.log(textWidth);
-    const borderLength =  textWidth;
-    input.style.width = `${borderLength}px`;
-  });
+  input.addEventListener("input", repaint);
+  input.addEventListener("submit", repaint);
 });
 
+function repaint(event) {
+  const input = event.target;
+  const font = window.getComputedStyle(input).font;
+  const enteredText = input.value;
+  const textWidth = getTextWidth(enteredText, font) || getTextWidth(input.placeholder, font);
+  console.log(textWidth);
+  console.log("HELLLOO");
+  const borderLength = textWidth;
+  input.style.width = `${borderLength}px`;
+}
+
 function getTextWidth(text, font) {
-  // Create a temporary canvas element
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
 
-  // Set the font for the context
   context.font = font;
 
-  // Measure the width of the text
   const metrics = context.measureText(text);
   const width = metrics.width;
 
-  // Clean up and remove the temporary canvas
   canvas.remove();
 
   return width;
--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,23 @@
 const API_KEY = "59091fe812c7a8fa3b500160aa99c1f7";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            const weather = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weather span:last-child");
-            city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp} degrees Celcius`;
+            city.innerText = `@ ${data.name}`;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp} °C`;
         });
 }
 
 function onGeoError() {
-    alert("Where are you...????");
+    weather.innerText = "Location Unknown";
 }
 
+weather.innerText = "Loading Weather...";
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
